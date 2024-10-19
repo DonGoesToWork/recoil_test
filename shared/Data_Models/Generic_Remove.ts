98313f14b09c4af9f20d2e6cc8dc46e0f7b3dadf
Donald <dongoestowork@outlook.com>
2024-10-19 14:58:50 -0400

    backup

--- shared/Data_Models/Generic_Remove.ts
+++ shared/Data_Models/Generic_Remove.ts
@@ -2,6 +2,5 @@ import { Pre_Message_Action_Send } from "../Communication/Communication_Interfac
 
 export interface IA_object_remove extends Pre_Message_Action_Send {
   object_class: string;
-  function_name: string;
   id: string;
 }
