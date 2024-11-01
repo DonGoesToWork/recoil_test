10ddcdbe8951ee4db07a3a4b34b8e406f77fabe9
Donald <dongoestowork@outlook.com>
2024-11-01 00:14:06 -0400

    updating more code gen stuff and cache

--- generator_tool/backend/src/index.ts
+++ generator_tool/backend/src/index.ts
@@ -16,17 +16,16 @@ const server = createServer(app);
 
 // Create Variables
 const root_project_path = "E:\\Web Dev\\2024_Projects\\recoil_test";
-const output_path = root_project_path + "\\generator_tool\\output\\";
 
-const backend_data_model_output_path = root_project_path + "\\generator_tool\\output\\backend_data_models\\";
-const frontend_data_model_output_path = root_project_path + "\\generator_tool\\output\\frontend_data_models\\";
-const shared_data_model_output_path = root_project_path + "\\generator_tool\\output\\shared_data_models\\";
-
-const object_registration_output_path = root_project_path + "\\generator_tool\\output\\object_registration_file\\";
-
-const notes_output_path = root_project_path + "\\generator_tool\\output\\note_data\\";
-
-const output_static_shared_path = root_project_path + "\\generator_tool\\output\\output_static_shared\\";
+let output_paths = {
+  base: root_project_path + "\\generator_tool\\output\\",
+  backend_data_model: root_project_path + "\\generator_tool\\output\\backend_data_models\\",
+  frontend_data_model: root_project_path + "\\generator_tool\\output\\frontend_data_models\\",
+  shared_data_model: root_project_path + "\\generator_tool\\output\\shared_data_models\\",
+  object_registration: root_project_path + "\\generator_tool\\output\\object_registration_file\\",
+  notes: root_project_path + "\\generator_tool\\output\\note_data\\",
+  output_static_shared: root_project_path + "\\generator_tool\\output_static_shared\\",
+};
 
 const state: Client_Message = {
   // Main state object
@@ -37,17 +36,14 @@ const state: Client_Message = {
 
 let ensure_exists = (dir: string) => {
   if (!fs.existsSync(dir)) {
-    fs.mkdirSync(dir);
+    fs.mkdirSync(dir, { recursive: true });
   }
 };
 
 let create_folders = () => {
-  ensure_exists(output_path);
-  ensure_exists(backend_data_model_output_path);
-  ensure_exists(frontend_data_model_output_path);
-  ensure_exists(shared_data_model_output_path);
-  ensure_exists(object_registration_output_path);
-  ensure_exists(notes_output_path);
+  for (const [key, value] of Object.entries(output_paths)) {
+    ensure_exists(value);
+  }
 };
 
 create_folders();
@@ -88,6 +84,22 @@ app.get("/import", (req, res) => {
   res.send(JSON.stringify(state.notes));
 });
 
+function copy_folder_to_folder(folder1: string, folder2: string) {
+  fs.readdir(folder1, (err: any, files: any[]) => {
+    if (err) {
+      console.error(err);
+    } else {
+      files.forEach((file) => {
+        fs.copyFile(folder1 + file, folder2 + file, (err: any) => {
+          if (err) {
+            console.error(err);
+          }
+        });
+      });
+    }
+  });
+}
+
 // Recieve and handle '/export' requests.
 app.post("/export", (req, res) => {
   console.log("Recieved export command.");
@@ -101,8 +113,8 @@ app.post("/export", (req, res) => {
 
   // * Delete current output folder.
 
-  if (fs.existsSync(output_path)) {
-    fs.rmSync(output_path, { recursive: true, force: true });
+  if (fs.existsSync(output_paths.base)) {
+    fs.rmSync(output_paths.base, { recursive: true, force: true });
   }
 
   create_folders(); // recreate empty folders.
@@ -112,7 +124,7 @@ app.post("/export", (req, res) => {
   // Write all of the main objects to .ts files.
   state.object_file_data.forEach((item) => {
     // write backend data model
-    fs.writeFile(backend_data_model_output_path + item.object_name + ".ts", item.backend_data_model, (err: any) => {
+    fs.writeFile(output_paths.backend_data_model + item.object_name + ".ts", item.backend_data_model, (err: any) => {
       if (err) {
         console.error(err);
         res.sendStatus(500); // return fail error code 500 to client.
@@ -120,7 +132,7 @@ app.post("/export", (req, res) => {
     });
 
     // write front-end data model.
-    fs.writeFile(frontend_data_model_output_path + item.object_name + ".ts", item.frontend_data_model, (err: any) => {
+    fs.writeFile(output_paths.frontend_data_model + item.object_name + ".ts", item.frontend_data_model, (err: any) => {
       if (err) {
         console.error(err);
         res.sendStatus(500); // return fail error code 500 to client.
@@ -128,7 +140,7 @@ app.post("/export", (req, res) => {
     });
 
     // write shared data model.
-    fs.writeFile(shared_data_model_output_path + item.object_name + ".ts", item.shared_data_model, (err: any) => {
+    fs.writeFile(output_paths.shared_data_model + item.object_name + ".ts", item.shared_data_model, (err: any) => {
       if (err) {
         console.error(err);
         res.sendStatus(500); // return fail error code 500 to client.
@@ -137,7 +149,7 @@ app.post("/export", (req, res) => {
   });
 
   // Write Object Registration file to .ts file.
-  fs.writeFile(object_registration_output_path + "/Object_Registration.ts", state.object_registration_contents, (err: any) => {
+  fs.writeFile(output_paths.object_registration + "/Object_Registration.ts", state.object_registration_contents, (err: any) => {
     if (err) {
       console.error(err);
       res.sendStatus(500); // return fail error code 500 to client.
@@ -145,7 +157,7 @@ app.post("/export", (req, res) => {
   });
 
   // Write our notes contents to a .ts file.
-  fs.writeFile(notes_output_path + "/notes.ts", JSON.stringify(state.notes), (err: any) => {
+  fs.writeFile(output_paths.notes + "/notes.ts", JSON.stringify(state.notes), (err: any) => {
     if (err) {
       console.error(err);
       res.sendStatus(500); // return fail error code 500 to client.
@@ -154,71 +166,40 @@ app.post("/export", (req, res) => {
 
   // * Move all files to their relevant locations within the frontend and backend folders one-level down from project root.
 
-  let frontend_loc = root_project_path + "\\frontend";
-  let frontend_src = root_project_path + "\\frontend\\src";
-  let frontend_generated = root_project_path + "\\frontend\\src\\z_generated";
-  let frontend_data_models = root_project_path + "\\frontend\\src\\z_generated\\Data_Models";
-  let frontend_shared = root_project_path + "\\frontend\\src\\z_generated\\Data_Models\\shared";
+  let frontend_data_models_path = root_project_path + "\\frontend\\src\\z_generated\\Data_Models\\";
+  let frontend_dm_shared_files_path = root_project_path + "\\frontend\\src\\z_generated\\Shared_Data_Models\\";
+  let frontend_shared_files_path = root_project_path + "\\frontend\\src\\z_generated\\Shared_Misc\\";
 
-  let backend_loc = root_project_path + "\\backend";
-  let backend_src = root_project_path + "\\backend\\src";
-  let backend_generated = root_project_path + "\\backend\\src\\z_generated";
-  let backend_data_models = root_project_path + "\\backend\\src\\z_generated\\Data_Models";
-  let backend_shared = root_project_path + "\\backend\\src\\z_generated\\Data_Models\\shared";
+  let backend_data_models_path = root_project_path + "\\backend\\src\\z_generated\\Data_Models\\";
+  let backend_dm_shared_files_path = root_project_path + "\\backend\\src\\z_generated\\Shared_Data_Models\\";
+  let backend_shared_files_path = root_project_path + "\\backend\\src\\z_generated\\Shared_Misc\\";
+
+  let object_registration_file_path = root_project_path + "\\backend\\src\\Object_Registration.ts";
 
   // Clear generated folders.
   fs.rmSync(root_project_path + "\\frontend\\src\\z_generated", { recursive: true, force: true });
   fs.rmSync(root_project_path + "\\backend\\src\\z_generated", { recursive: true, force: true });
 
-  // Put files (item.backend_data_model) in backend_data_model_output_path into the root_project_path/backend/src/Data_Models folder.
-  state.object_file_data.forEach((item) => {
-    fs.copyFile(backend_data_model_output_path + item.object_name + ".ts", backend_data_models + item.object_name + ".ts", (err: any) => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500); // return fail error code 500 to client.
-      }
-    });
-  });
+  ensure_exists(backend_data_models_path);
+  copy_folder_to_folder(output_paths.backend_data_model, backend_data_models_path);
 
-  // Put front-end files in their proper location too:
-  state.object_file_data.forEach((item) => {
-    fs.copyFile(frontend_data_model_output_path + item.object_name + ".ts", frontend_data_models + item.object_name + ".ts", (err: any) => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500); // return fail error code 500 to client.
-      }
-    });
-  });
+  ensure_exists(frontend_data_models_path);
+  copy_folder_to_folder(output_paths.frontend_data_model, frontend_data_models_path);
 
-  // Put shared files into the two shared folders.
-  state.object_file_data.forEach((item) => {
-    fs.copyFile(shared_data_model_output_path + item.object_name + ".ts", frontend_shared + item.object_name + ".ts", (err: any) => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500); // return fail error code 500 to client.
-      }
-    });
-  });
-  state.object_file_data.forEach((item) => {
-    fs.copyFile(shared_data_model_output_path + item.object_name + ".ts", backend_shared + item.object_name + ".ts", (err: any) => {
-      if (err) {
-        console.error(err);
-        res.sendStatus(500); // return fail error code 500 to client.
-      }
-    });
-  });
+  ensure_exists(frontend_dm_shared_files_path);
+  copy_folder_to_folder(output_paths.shared_data_model, frontend_dm_shared_files_path);
 
-  // Write Object Registration file to .ts file on backend at [ root_project_path + "\\backend\\src\\ObjectRegistration.ts" ].
-  fs.copyFile(object_registration_output_path + "/Object_Registration.ts", root_project_path + "\\backend\\src\\Object_Registration.ts", (err: any) => {
-    if (err) {
-      console.error(err);
-      res.sendStatus(500); // return fail error code 500 to client.
-    }
-  });
+  ensure_exists(backend_dm_shared_files_path);
+  copy_folder_to_folder(output_paths.shared_data_model, backend_dm_shared_files_path);
 
-  // Write the folders in the 'output_static_shared' folder to the frontend and backend to the shared paths.
-  fs.copySync(output_static_shared_path, root_project_path + "\\frontend\\src\\z_generated\\Data_Models\\shared");
-  fs.copySync(output_static_shared_path, root_project_path + "\\backend\\src\\z_generated\\Data_Models\\shared");
+  ensure_exists(object_registration_file_path);
+  copy_folder_to_folder(output_paths.object_registration, object_registration_file_path);
+
+  ensure_exists(frontend_shared_files_path);
+  copy_folder_to_folder(output_paths.output_static_shared, frontend_shared_files_path);
+
+  ensure_exists(backend_shared_files_path);
+  copy_folder_to_folder(output_paths.output_static_shared, backend_shared_files_path);
 
   // All files written successfully, send success return code to client with 200.
   res.sendStatus(200);
@@ -229,12 +210,12 @@ server.listen(PORT, () => {
   console.log(`Server is loading existing class objects from files.`);
 
   // Load Note Data from files in notes_output_path.
-  fs.readdir(notes_output_path, (err: any, files: any[]) => {
+  fs.readdir(output_paths.notes, (err: any, files: any[]) => {
     if (err) {
       console.error(err);
     } else {
       files.forEach((file) => {
-        fs.readFile(notes_output_path + file, "UTF-8", (err: any, data: any) => {
+        fs.readFile(output_paths.notes + file, "UTF-8", (err: any, data: any) => {
           if (err) {
             console.error(err);
           } else {
--- shared/Data_Models/Class_Names.ts
+++
@@ -1,4 +0,0 @@
-export const CLASS_NAME_FARMER: string = "Farmer";
-export const CLASS_NAME_BEE_FARM: string = "Bee_Farm";
-export const CLASS_NAME_BEE_HIVE: string = "Bee_Hive";
-export const CLASS_NAME_BEE: string = "Bee";
