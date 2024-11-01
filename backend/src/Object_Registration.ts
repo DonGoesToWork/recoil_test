41c0ae18349f82152d81f75ec02e5c068550232c
Donald <dongoestowork@outlook.com>
2024-11-01 00:19:13 -0400

    fix  object_registration file creation

---
+++ backend/src/Object_Registration.ts
@@ -0,0 +1,23 @@
+import Backend_State from "./static_internal_logic/Backend_State";
+import { Pre_Message_Action_Send } from "./z_generated/Shared_Misc/Communication_Interfaces";
+import { Register_Bee } from "./z_generated/Data_Models/Bee";
+import { Register_Bee_Farm } from "./z_generated/Data_Models/Bee_Farm";
+import { Register_Bee_Hive } from "./z_generated/Data_Models/Bee_Hive";
+import { Register_Farmer } from "./z_generated/Data_Models/Farmer";
+import { Register_Player } from "./z_generated/Data_Models/Player";
+
+export interface Class_Function {
+  (message_action: Pre_Message_Action_Send, state: Backend_State): void;
+}
+
+export interface Object_Class_Function_Map {
+  [key: string]: { [key: string]: Class_Function };
+}
+
+export let Register_Objects = (x: Object_Class_Function_Map): void => {
+  Register_Bee(x);
+  Register_Bee_Hive(x);
+  Register_Bee_Farm(x);
+  Register_Farmer(x);
+  Register_Player(x);
+};
